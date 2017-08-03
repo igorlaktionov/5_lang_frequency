fb77798689bf8aaab0aa308eca3cbe3bb919f243
Igor Laktionov <ilaktionov@iponweb.net>
2017-08-03 11:06:41 +0300

    added most frequent words

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# Frequency Analysis of Words
+# Популярные слова
 
-[TODO. There will be project description]
+Скрипт для нахождения самых популярных слов в тексте
 
-# Project Goals
+# Как запустить
+
+Скрипт требует для своей работы установленного интерпретатора Python версии 3.5
+
+Запуск на Linux:
+
+```#!bash
+
+$ python lang_frequency.py -f=path/to/file # possibly requires call of python3 executive instead of just python
+
+Пример вывода:
+The
+world
+main
+...
+```
+
+Запуск на Windows происходит аналогично.
+
+# Цели проекта
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,41 @@
+import argparse
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    try:
+        return open(filepath, 'r').read().lower()
+    except OSError as error:
+        print(error)
+        return None
 
 
-def get_most_frequent_words(text):
-    pass
+def get_most_frequent_words(text, number_most_popular_words):
+    try:
+        words = re.findall(r'\w+', text)
+        return Counter(words).most_common(number_most_popular_words)
+    except TypeError as error:
+        print(error)
+        return None
+
+
+def print_most_frequent_words(most_frequent_words):
+    if most_frequent_words and isinstance(most_frequent_words, list):
+        for word, qty in most_frequent_words:
+            print(word)
+
+
+def parser_input_data():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-filepath', '-f', dest='file_path', required=True, type=str, help='-filepath=/path/to/file')
+    parser.add_argument('-number', dest='number_most_popular_words', type=int, default=10)
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    args = parser_input_data()
+    text = load_data(args.file_path)
+    if text:
+        most_frequent_words = get_most_frequent_words(text, args.number_most_popular_words)
+        print_most_frequent_words(most_frequent_words)
